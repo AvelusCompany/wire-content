bcfd5325716c103ff784422ed65579be4f8ea251
SSAvelu <151690345+SSAvelu@users.noreply.github.com>
2024-10-17 12:06:48 -0600

    Create Article model in wire-content package for managing article data

--- src/Models/Article.php
+++ src/Models/Article.php
@@ -6,10 +6,12 @@ use Awcodes\Curator\Models\Media;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use WireComments\Traits\Commentable;
 
 class Article extends Model
 {
     use HasFactory;
+    use Commentable;
 
     protected $fillable = [
         'title',
